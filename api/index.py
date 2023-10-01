428e79b0e98160d04033cc0a3e5f9de4e7caa4b9
Hilbelinka5142 <144185910+Hilbelinka5142@users.noreply.github.com>
2023-10-01 09:14:31 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Vercel demo</title>
-</head>
-<body>
-    <h1>Hello {{ data }}</h1>
-</body>
-</html>
+from requests import get
+
+ip = get('https://api.ipify.org').text
+print('My public IP address is: {}'.format(ip))

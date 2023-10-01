c89591511ec0a043d723c3ab61ccf468333443bb
Hilbelinka5142 <Hilbelinka5142@my.uwstout.edu>
2023-10-01 16:10:09 +0000

    changed api

--- api/index.py
+++ api/index.py
@@ -1,13 +1,22 @@
+#from requests import get
+   # ip = get('https://api.ipify.org').text
+   # print('My public IP address is: {}'.format(ip))
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Module 4 project</title>
-</head>
-<body>
-    <h1>Hello {
-    from requests import get
-    ip = get('https://api.ipify.org').text
-    print('My public IP address is: {}'.format(ip))}</h1>
-</body>
-</html>
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    className = 'cnit 381'
+
+    return render_template('index.html', data=className)
+
+@app.route('/about')
+def about():
+    return 'About'
+
+@app.route('/another')
+def another():
+    return 'Another'

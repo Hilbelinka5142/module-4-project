c760ec8737d999e7a41b8c7202f2388da5198cac
Hilbelinka5142 <144185910+Hilbelinka5142@users.noreply.github.com>
2023-10-01 09:12:38 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,18 +1,9 @@
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    className = 'cnit 381'
-
-    return render_template('index.html', data=className)
-
-@app.route('/about')
-def about():
-    return 'About'
-
-@app.route('/another')
-def another():
-    return 'Another'
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vercel demo</title>
+</head>
+<body>
+    <h1>Hello {{ data }}</h1>
+</body>
+</html>

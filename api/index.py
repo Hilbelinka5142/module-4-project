3f8bae3b35081341df200ac0af0259c3f15c091c
Hilbelinka5142 <144185910+Hilbelinka5142@users.noreply.github.com>
2023-09-30 22:38:38 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
-from requests import get
+#from requests import get
 
-ip = get('https://api.ipify.org').text
-print('My public IP address is: {}'.format(ip))
+#ip = get('https://api.ipify.org').text
+#print('My public IP address is: {}'.format(ip))
 
 from flask import Flask, render_template
 

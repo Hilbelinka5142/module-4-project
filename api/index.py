2cb9216de0b5fbbce6259858d5edca1871e0966f
Hilbelinka5142 <Hilbelinka5142@my.uwstout.edu>
2023-10-01 06:13:07 +0000

    updated api

--- api/index.py
+++ api/index.py
@@ -1,13 +1,13 @@
-#from requests import get
 
-#ip = get('https://api.ipify.org').text
-#print('My public IP address is: {}'.format(ip))
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Vercel demo</title>
+    <title>Module 4 project</title>
 </head>
 <body>
-    <h1>Hello {{ data }}</h1>
+    <h1>Hello {
+    from requests import get
+    ip = get('https://api.ipify.org').text
+    print('My public IP address is: {}'.format(ip))}</h1>
 </body>
 </html>

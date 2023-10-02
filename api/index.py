e6a95e83854a40ffcb3615c661323ee71138e777
Hilbelinka5142 <144185910+Hilbelinka5142@users.noreply.github.com>
2023-10-02 09:59:29 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,14 @@
 from requests import get
 
-class handler():
-    def home():
-        ip = get('https://api.ipify.org').text
-        data = print('My public IP address is: {}'.format(ip))
-        return data
+app = get(__name__)
+
+@app.route('/')
+def home():
+    ip = get('https://api.ipify.org').text
+    data = print('My public IP address is: {}'.format(ip))
+    return data
+
+@app.route('/about')
+def about():
+    return 'about'
     

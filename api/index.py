88f0263ae61a709e13781afd7d88e13c0ef6be25
Hilbelinka5142 <144185910+Hilbelinka5142@users.noreply.github.com>
2023-10-02 09:25:27 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,17 +1,4 @@
-from flask import Flask, render_template
+from requests import get
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    className = 'cnit 381'
-
-    return render_template('index.html', data=className)
-
-@app.route('/about')
-def about():
-    return 'About'
-
-@app.route('/another')
-def another():
-    return 'Another'
+ip = get('https://api.ipify.org').text
+print('My public IP address is: {}'.format(ip))

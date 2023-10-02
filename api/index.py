ec28526539a57a004e52b712325d96802c3447e4
Hilbelinka5142 <144185910+Hilbelinka5142@users.noreply.github.com>
2023-10-02 09:42:21 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
-from requests import get
+from requests import get, render_template
+
 
 app = get(__name__)
 
+@app.route('/')
 def home():
     ip = get('https://api.ipify.org').text
     return ('My public IP address is: {}'.format(ip))

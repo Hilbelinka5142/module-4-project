929b8796f73c7ab3d1924e013f7d632eb8fb5bce
Hilbelinka5142 <144185910+Hilbelinka5142@users.noreply.github.com>
2023-10-02 09:32:28 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,5 @@
 from requests import get
-<<<<<<< HEAD
   
 ip = get('https://api.ipify.org').text
 print('My public IP address is: {}'.format(ip))
 
-=======
-
-ip = get('https://api.ipify.org').text
-print('My public IP address is: {}'.format(ip))
->>>>>>> 428e79b0e98160d04033cc0a3e5f9de4e7caa4b9

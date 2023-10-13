3c32bed28597f2e114cdcce0f990fc32af67f247
Hilbelinka5142 <144185910+Hilbelinka5142@users.noreply.github.com>
2023-10-13 11:47:21 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 from requests import get
 
-app = get(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    ip = requests.get('https://api.ipify.org').text
+    ip = requests.Flask('https://api.ipify.org').text
     data = 'My public IP address is: {}'.format(ip)
     return print('index.html', data)
 
